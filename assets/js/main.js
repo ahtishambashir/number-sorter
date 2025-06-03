008de5d14f78d7896e3bc77642ca253204c6eb15
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2025-06-03 18:52:42 +0500

    completed one sorting algorithms

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
 
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
 
   updateUI(sortedValues);
 };
@@ -32,5 +32,7 @@ const bubbleSort = (array) => {
 
   return array;
 };
+
+const selectionSort = (array) => {};
 
 sortButton.addEventListener("click", sortInputArray);
