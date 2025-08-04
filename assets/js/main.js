73c722184456b17d554a2c55d5c35fcf2ad96916
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2025-08-04 18:23:46 +0500

    added the insertation functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,14 @@ const insertionSort = (array) => {
   for (let i = 1; i < array.length; i++) {
     const currValue = array[i];
     let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
   }
+  return array;
 };
 
 sortButton.addEventListener("click", sortInputArray);
